0ffa52005f5288d8d0de4df6d554e0a31409c5f4
belvvika <134885414+belvvika@users.noreply.github.com>
2024-04-09 23:29:25 +0300

    new

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -1,12 +1,12 @@
 
 from django.core.management import BaseCommand
 
-from catalog.models import Product, Category
+from catalog.models import Product
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         product_list = [
-            {'name': 'milk', 'title': 'milk for family', 'picture': 'Null', 'category': 'category', 'cost': 100, 'date_of_create': 01.01.2004, 'last_modified_date': 01.02.2004}
+            {'name': 'milk', 'title': 'milk for family', 'picture': 'Null', 'category': 'category', 'cost': 100, 'date_of_create': 2004-1-1, 'last_modified_date': 2004-1-2}
         ]
 
         product = []
--- catalog/models.py
+++ catalog/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='Наименование')
     title = models.TextField(max_length=150, verbose_name='Описание')
     picture = models.ImageField(verbose_name='Изображение')
-    category = models.CharField(max_length=50, verbose_name='Категория')
+    category = models.ForeignKey('Product', on_delete=models.CASCADE, verbose_name='Категория')
     cost = models.IntegerField(verbose_name='Цена за штуку')
     date_of_create = models.DateField(verbose_name='Дата создания')
     last_modified_date = models.DateField(verbose_name='Дата последнего изменения')
