2ee42ad41e724e5022114be4e141d27ca7343fdc
belvvika <134885414+belvvika@users.noreply.github.com>
2024-07-03 11:46:40 +0300

    new

--- config/urls.py
+++ config/urls.py
@@ -24,7 +24,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('catalog.urls', 'catalog'), namespace='catalog')),
-    path('', include(('blog.urls', 'blog'), namespace='blog'))
+    path('blog/', include(('blog.urls', 'blog'), namespace='blog'))
 ]
 
 if settings.DEBUG:
