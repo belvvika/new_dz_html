734cc9d112ca20d7251642f3b31c2a9fa325a4ec
belvvika <134885414+belvvika@users.noreply.github.com>
2024-08-01 11:09:04 +0300

    new

--- catalog/forms.py
+++ catalog/forms.py
@@ -7,6 +7,12 @@ class ProductForm(ModelForm):
         model = Product
         fields = '__all__'
 
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        if name == 'казино' or 'криптовалюта' or 'крипта' or 'биржа' or 'дешево' or 'бесплатно' or 'обман' or 'полиция' or 'радар':
+            raise forms.ValidationError('Недопустимое название товара')
+        return name
+
 class VersionForm(ModelForm):
     class Meta:
         model = Version
